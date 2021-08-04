f4961c59465f7aa30f695658bb9f045fc49c29c0
sarvenaz-srv <sarvghad79@gmail.com>
2021-08-04 19:12:33 +0430

    fix some links

--- src/store.js
+++ src/store.js
@@ -6,8 +6,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        api: 'https://aaiss.ceit.aut.ac.ir/api/2020',
-        mediaRoot:'https://aaiss.ceit.aut.ac.ir',
+        api: 'https://aaiss.ce.aut.ac.ir/api/2020',
+        mediaRoot:'https://aaiss.ce.aut.ac.ir',
         // api: 'http://localhost:8000/api',
         // media: 'http://localhost:8000',
 
